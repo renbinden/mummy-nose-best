ff8d3234d95287a65708632f01c3d8830961fc36
ross <ross@seventh-root.com>
2016-01-30 16:11:55 +0000

    Adds players, cursors, and gamepad support

--- core/src/com/seventh_root/mummy_nose_best/MainScreen.java
+++ core/src/com/seventh_root/mummy_nose_best/MainScreen.java
@@ -9,14 +9,26 @@ import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 public class MainScreen extends ScreenAdapter {
 
     SpriteBatch spriteBatch;
+    PlayerManager playerManager;
 
     public MainScreen() {
         spriteBatch = new SpriteBatch();
+        playerManager = new PlayerManager();
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL_COLOR_BUFFER_BIT);
+        spriteBatch.begin();
+        playerManager.renderPlayers(delta, spriteBatch);
+        spriteBatch.end();
     }
+
+    @Override
+    public void dispose() {
+        spriteBatch.dispose();
+        playerManager.dispose();
+    }
+
 }
