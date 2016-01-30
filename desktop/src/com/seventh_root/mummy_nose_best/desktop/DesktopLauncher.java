e4539222ef179c5d6216e0322a92e86cb31a7478
ross <ross@seventh-root.com>
2016-01-30 12:34:12 +0000

    Switch from Phaser to libgdx

---
+++ desktop/src/com/seventh_root/mummy_nose_best/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.seventh_root.mummy_nose_best.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.seventh_root.mummy_nose_best.MummyNoseBest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MummyNoseBest(), config);
+	}
+}
---
+++ html/src/com/seventh_root/mummy_nose_best/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.seventh_root.mummy_nose_best.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.seventh_root.mummy_nose_best.MummyNoseBest;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MummyNoseBest();
+        }
+}
