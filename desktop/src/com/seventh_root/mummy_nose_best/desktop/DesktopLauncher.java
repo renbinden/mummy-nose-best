872ac7fcf304a41fa12c9235a3217e86d2bffb1e
ross <ross@seventh-root.com>
2016-01-30 13:09:59 +0000

    Changes application size to 800x600

--- desktop/src/com/seventh_root/mummy_nose_best/desktop/DesktopLauncher.java
+++ desktop/src/com/seventh_root/mummy_nose_best/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.seventh_root.mummy_nose_best.MummyNoseBest;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		new LwjglApplication(new MummyNoseBest(), config);
 	}
 }
